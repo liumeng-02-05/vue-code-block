7c26cd339ae90cbc9057dcb83c5b5825b7298ff2
liumeng <1808728912@qq.com>
2022-02-24 19:30:52 +0800

    修改 vue2 应用

--- vue2-demo/src/index.vue
+++ vue2-demo/src/index.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>
-      vue2-代码片段
+  <div @click="changeValue">
+    vue-demo1
+    <div>{{mesage}}</div>
   </div>
 </template>
+
+<script>
+  export default {
+    data(){
+      const mesage = 'hellow vue-block'
+      return {
+        mesage
+      }
+    },
+    methods:{
+      changeValue (){
+        this.mesage = '改变message的值'
+      }
+    }
+  }
+</script>
+
+<style  scoped>
+
+</style>
